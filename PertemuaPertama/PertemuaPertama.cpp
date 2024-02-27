fa9c83cd2cd756814a74882ee3f095ca7cf18379
fadd00 <andhikahutama9@gmail.com>
2024-02-27 18:10:42 +0700

    luas persegi

--- PertemuaPertama/PertemuaPertama.cpp
+++ PertemuaPertama/PertemuaPertama.cpp
@@ -20,4 +20,15 @@ int main()
     cin >> nTinggi;
     nLuas = nAlas * nTinggi / 2;
     cout << "Luasnya = " << nLuas;
+
+
+    int panjang, lebar, luas;
+    cout << "Masukan Panjang = ";
+    cin >> panjang;
+    cout << "Masukan Lebar = ";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luas Persegi Panjang adalah = " << luas;
+
+
 }
